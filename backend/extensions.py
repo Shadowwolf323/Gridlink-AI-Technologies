2a004dfe5885a92af8c8d28cc043c80e4d067176
Angel Knowledge Shingube <224068911@edu.vut.ac.za>
2026-08-25 17:48:47 +0200

    Remove duplicate import of SQLAlchemy

--- backend/extensions.py
+++ backend/extensions.py
@@ -2,8 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from flask_sqlalchemy import SQLAlchemy
-
 
 db = SQLAlchemy()
 migrate = Migrate()
